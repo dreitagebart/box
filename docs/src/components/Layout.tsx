eaabd781e254539f6686f47fde6a36b4fb8cb9aa
dreitagebart <dreitagebart@users.noreply.github.com>
2020-08-14 15:53:54 +0200

    added gatsby docs

---
+++ docs/src/components/Layout.tsx
@@ -0,0 +1,38 @@
+import React, { Fragment } from "react"
+import { useStaticQuery, graphql } from "gatsby"
+
+import { Header, GlobalStyle } from "./"
+import "./layout.css"
+
+export const Layout: React.FC = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  return (
+    <Fragment>
+      <GlobalStyle></GlobalStyle>
+      <Header siteTitle={data.site.siteMetadata.title} />
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `0 1.0875rem 1.45rem`,
+        }}
+      >
+        <main>{children}</main>
+        <footer>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        </footer>
+      </div>
+    </Fragment>
+  )
+}
